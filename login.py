ba59161051dafe2a0c85132341e40f61fb1e0220
GZSCloud <trustds@163.com>
2019-05-10 10:42:34 +0800

    将需要用户输入xpath，改为输入界面元素和组件名称。还需要修改为仅仅输入界面元素。

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-# @Time  : 2019/4/28 22:11
+# @Time  : 2019/5/10 22:11
 # @功能  ：利用selenium实现简单的提交表单类型的自动化测试
 from selenium import webdriver
 import json
@@ -12,44 +12,52 @@ def login(jsonData):
     :param jsonData: json标准格式的字符串
     :return: True/False 在响应的页面中匹配judge_words成功返回True，不成功返回False
     """
+    ##检查json串格式
 
     # 解析json字符串，转换成相应的数据结构
     data = json.loads(jsonData)
 
     # 打开网页
     browser.get(data['url'])
+    ##key写成list,再从list读出
+    #对moudles中的key解析成相应的xpath语句
+    for k,v in data['moudles'].items():
+        data['moudles'][k]="//input[@name='%s']"% v
 
-    # 根据xpath语句和data实现自动定位到输入框并输入数据
-    # 支持一个网页中任意多个输入框的输入
-    if str(type(data['xpath'])) == "<class 'list'>":
-        for i in range(0,len(data['xpath'])):
-            name = browser.find_element_by_xpath(data['xpath'][i])
-            name.clear()
-            name.send_keys(data['data'][i])
-    else:
-        item = browser.find_element_by_xpath(data['xpath'])
-        item.clear()
-        item.send_keys(data['data'])
+    #对最后click的key解析成相应的xpath语句
+    for k,v in data['click'].items():
+        data['click'][k]="//input[@type='%s']"% v
 
-    #  查找提交按钮并点击
-    browser.find_element_by_xpath(data['xpath_submit']).click()
+    # 根据key和data实现自动定位到输入框并输入数据,支持一个网页中任意多个输入框的输入
+    for i in range(0,len(data['moudles'])):
+        name = browser.find_element_by_xpath(list(data['moudles'].values())[i])
+        name.clear()
+        name.send_keys(list(data['data'].values())[i])
 
-    # 在网页源码中匹配相应的关键字，判断是否登陆成功
+    # 点击按钮
+    browser.find_element_by_xpath(list(data['click'].values())[0]).click()
+
+
+    # 在返回的网页源码中匹配相应的关键字，判断是否登陆成功
     if browser.page_source.find(data["judge_words"]) != -1:
         return False
     else:
         return True
 
-
+# ["登录":"login","查询":"browse","退出","exit"]
 # 标准json字符串格式，以后将支持从数据库中读取关键字自动生成json格式的字符串数组
-JsonData = ["""{
+JsonDatas = ["""{
     "url":"https://github.com/login",
-    "xpath":["//input[@name='login'or@type='text']","//input[@type='password']"],
-    "data": ["abcd","1234"],
-    "xpath_submit":"//input[@type='submit']",
+    "moudles":{"登录名":"login","密码":"password"},
+    "data": {"登录名":"abcd","密码":"1234"},
+    "click":{"登录":"submit"},
     "judge_words":"Incorrect username or password."
 }"""]
 
+# 可以用testdata存放测试用例,写一个方法自动生成JsonDatas
+
+
+
 if __name__ == '__main__':
     # 由于在python3中map输出的是可迭代对象，可以用list()让它自动迭代运行，否则不会运行函数
-    print(list(map(login, JsonData)))
+    print(list(map(login, JsonDatas)))
